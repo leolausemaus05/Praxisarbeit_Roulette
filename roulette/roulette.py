bc3ec78d8c4ead73fd8f4e5edb70a16bbd5ffb5a
Tim Weishaupt <tim.weishaupt06@web.de>
2026-03-13 10:23:25 +0100

    Erste Abfrage_test

--- roulette/roulette.py
+++ roulette/roulette.py
@@ -1,3 +1,9 @@
 possibillity_1= "red"
 possibillity_2= "black"
 possibillity_3= "green"
+
+print("Type in "Start" to start the Roullete Game")
+
+if input() == "Start":
+    print("How much money do you want to bet?")
+    bet = int(input("€"))
