bc0076b21dc11fa3870c316302462d13e1e8b893
Tim Weishaupt <tim.weishaupt06@web.de>
2026-03-13 10:40:11 +0100

    Erweiterung des Programms um die Bet Funktion

--- roulette/roulette.py
+++ roulette/roulette.py
@@ -7,5 +7,10 @@ Eingabe_1= "Start"
 print("Type in " + Eingabe_1 + " to start the Roullete Game")
 
 if input() == Eingabe_1:
-    print("How much money do you want to bet?")
-    bet = int(input("€"))
+    print("How much euros do you want to bet?")
+    bet = int(input(""))
+
+else:
+    print("You have to type in " + Eingabe_1 + " to start the game")
+
+print("On what color do you want to bet on? Red or Black or Green?")
