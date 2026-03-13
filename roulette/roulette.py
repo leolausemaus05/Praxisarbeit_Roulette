c22ed78f5f9170b64e3694902a8f38934277f5c4
Tim Weishaupt <tim.weishaupt06@web.de>
2026-03-13 10:26:40 +0100

    Verbesserung

--- roulette/roulette.py
+++ roulette/roulette.py
@@ -2,8 +2,10 @@ possibillity_1= "red"
 possibillity_2= "black"
 possibillity_3= "green"
 
-print("Type in "Start" to start the Roullete Game")
+Eingabe_1= "Start"
 
-if input() == "Start":
+print("Type in " + Eingabe_1 + " to start the Roullete Game")
+
+if input() == Eingabe_1:
     print("How much money do you want to bet?")
     bet = int(input("€"))
