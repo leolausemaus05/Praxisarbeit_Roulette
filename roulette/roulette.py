3921197f2a9fe22fdaaef7068a9a21e0e0e232e6
Tim Weishaupt <tim.weishaupt06@web.de>
2026-03-15 16:58:31 +0100

    Erweiterung um farbige Schrift

--- roulette/roulette.py
+++ roulette/roulette.py
@@ -4,15 +4,29 @@ possibillity_3= "green"
 
 Zahlen = (0-36)
 
-Eingabe_1= "Start"
+Eingabe_1= "Go"
 
-print("Type in " + Eingabe_1 + " to start the Roullete Game")
+print("Type in " + Eingabe_1 + " to start the Roulette Game")
 
 if input() == Eingabe_1:
     print("How much euros do you want to bet?")
     bet = int(input(""))
+    print("You bet " + str(bet) + " euros!")
 
 else:
     print("You have to type in " + Eingabe_1 + " to start the game")
 
-print("On what color do you want to bet on? Red or Black or Green?")
+print("On what color do you want to bet on? \033[31m red\033[0m or \033[30m black\033[0m or \033[32m green\033[0m?")
+
+if input() == possibillity_1:
+    print("You bet on \033[31m red\033[0m!")
+
+elif input() == possibillity_2:
+    print("You bet on \033[30m black\033[0m!")
+
+elif input() == possibillity_3:
+    print("You bet on \033[32m green\033[0m!")
+
+else:
+    print("Invalid color choice!")
+
