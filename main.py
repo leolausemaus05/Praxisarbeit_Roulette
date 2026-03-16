1208fb267a38f581ce172c81e679efeda7c88b9b
Leo Mayer <leo.mayer@t-online.de>
2026-03-16 22:11:42 +0100

    Drittel funktion eingefügt, Allgemeine Überarbeitung

--- main.py
+++ main.py
@@ -7,73 +7,83 @@ Beschreibung: Vereinfachte Version des Glückspiels Roulette
 
 """
 
-from gamelogic import wheel_spin, define_color, check_color, check_number, farben, zahlen
+from gamelogic import wheel_spin, define_color, check_color, check_number, farben, zahlen, check_drittel
 
 possibility_1 = "red"
 possibility_2 = "black"
 possibility_3 = "green"
 
 # Spiel starten
-start = "Go"
-print("Type in " + start + " to start the Roulette Game:")
+start = "Start"
 
-check = input()
+check = input("Geben Sie 'Start' ein, um das Spiel zu beginnen: ")
 
 if check == start:
 
-    credit = int(input("How many euros do you want to bet? "))
+    credit = int(input("Wie viele Coins möchten Sie setzen? "))
 
     # Spielschleife, läuft solange Guthaben vorhanden ist
     while credit > 0:
     # Menüauswahl
-        print("Game on...")
-        print("Your credit is:", credit)
-        print("1 = Bet on color")
-        print("2 = Bet on number")
-        print("4 = End game")
-        print("3 = Bet on range of numbers")
+        print("Spiel gestartet...")
+        print("Ihr Guthaben beträgt:", credit , "Coins")
+        print("1 = Auf eine Farbe setzen")
+        print("2 = Auf eine Zahl setzen")
+        print("3 = Auf ein Drittel der Zahlen setzen")
+        print("4 = Spiel beenden")
 
-        choice = input("Your choice: ")
+        choice = input("Bitte wählen Sie ihre Option: ")
 
         # Farbwette
         if choice == "1":
-            print("Available colors:")
+            print("Sie können auf folgende Farben setzen:")
             print(possibility_1, possibility_2, possibility_3)
 
-            color = input("On what color do you want to bet? ").lower()
+            color = input("Auf welche Farbe möchten Sie wetten? (red, black, green) ").lower()
             
             result = wheel_spin()
-            print("You bet on", color)
-            print("Result:", result, define_color(result))
+            print("Sie haben auf", color, "gewettet")
+            print("Ergebnis:", result, define_color(result))
 
             if check_color(color, result):
-                print("You won!")
-                credit = credit + credit
+                print("Sie haben gewonnen!")
+                credit = credit * 2
             else:
-                print("you lost!")
+                print("Sie haben verloren!")
                 credit = credit - credit
 
         # Zahlenwette
         elif choice == "2":
-            print ("Available numbers: 0 - 36")
+            print ("Sie können auf folgende Zahlen setzen: 0 - 36")
 
-            number = int(input("On what number do you want to bet? "))
+            number = int(input("Auf welche Zahl möchten Sie wetten? "))
 
             result = wheel_spin()
-            print("You bet on", number)
-            print("Result:", result, define_color(result))
+            print("Sie haben auf", number, "gewettet")
+            print("Ergebnis:", result, define_color(result))
 
             if check_number(number, result):
-                print("You won!")
-                credit = credit + credit
+                print("Sie haben gewonnen!")
+                credit = credit * 36
             else:
-                print("You lost!")
+                print("Sie haben verloren!")
                 credit = credit - credit
 
         # Auf Range der Zahlen setzen
         elif choice == "3":
-            print("Available ranges: 1 - 12, 13 - 24, 25 - 36")
+            print("Sie können auf folgende Drittel setzen: 1 - 12, 13 - 24, 25 - 36")
 
+            drittel = input("Auf welches Drittel möchten Sie setzen? (1, 2 oder 3) ")
+            result = wheel_spin()
+            print("Sie haben auf Drittel", drittel, "gewettet") 
+            print("Ergebnis:", result, define_color(result))    
+
+            if check_drittel(drittel, result):
+                print("Sie haben auf das richtige Drittel gesetzt, Sie haben gewonnen!")
+                credit = credit * 3
+            else:
+                print("Sie haben verloren!")
+                credit = credit - credit
 
         # Spiel beenden
         elif choice == "4":
